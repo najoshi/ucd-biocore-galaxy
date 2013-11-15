943a0ecaaabe8776cc3781ad7bc016467868a525
root <root@ip-10-196-105-206.us-west-1.compute.internal>
2013-11-15 14:05:22 +0000

    making changes to split to work better, adding multi bam file capability to HaplotypeCaller

--- tools/filters/split_bam_file.pl
+++ tools/filters/split_bam_file.pl
@@ -21,10 +21,16 @@ foreach $hd (split (/\n/, $headers)) {
 	}
 }
 
+open ($outf, ">$outfile1");
+print $outf "# Log File\n\nHeaders found:\n$headers\nSplitting BAM file into multiple files.\nNote: You may need to refresh history multiple times to get all the output files.\n";
+close ($outf);
+
 system ("mkdir -p $newfilepath");
 open ($tempscript, ">$tempfilepath/tempscript.sh");
 foreach $chr (@chrs) {
-	print $tempscript "/share/apps/samtools/samtools view -b -o $newfilepath/primary_$outfileid"."_$chr"."_visible_bam file.bam $chr &\n";
+	$chrsub = $chr;
+	$chrsub =~ s/_/-/g;
+	print $tempscript "/share/apps/samtools/samtools view -b -o $newfilepath/primary_$outfileid"."_$chrsub"."_visible_bam file.bam $chr &\n";
 }
 
 print $tempscript "wait\n";
