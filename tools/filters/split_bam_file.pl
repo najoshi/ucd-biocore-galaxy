0ba1de4894b58ee6af14c83784d4421ea411c9e0
Nikhil Joshi <najoshi@ucdavis.edu>
2013-11-20 03:04:48 +0000

    changed split bam to use job working directory

--- tools/filters/split_bam_file.pl
+++ tools/filters/split_bam_file.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/perl
 
-($bamfile, $bamindex, $outfile1, $outfileid, $newfilepath, $tempfilepath) = @ARGV;
-
-system ("mkdir -p $tempfilepath");
-chdir ("$tempfilepath");
+($bamfile, $bamindex, $outfile1, $outfileid) = @ARGV;
 
 system ("ln -s $bamindex file.bam.bai");
 system ("ln -s $bamfile file.bam");
@@ -11,7 +8,6 @@ $headers = `/share/apps/samtools/samtools view -H file.bam`;
 
 if ($headers eq "") {
 	print STDERR "Error: No headers detected in BAM file. Aborting.\n";
-    system ("rm -rf $tempfilepath");
 	exit(1);
 }
 
@@ -25,16 +21,14 @@ open ($outf, ">$outfile1");
 print $outf "# Log File\n\nHeaders found:\n$headers\nSplitting BAM file into multiple files.\nNote: You may need to refresh history multiple times to get all the output files.\n";
 close ($outf);
 
-system ("mkdir -p $newfilepath");
-open ($tempscript, ">$tempfilepath/tempscript.sh");
+open ($tempscript, ">tempscript.sh");
 foreach $chr (@chrs) {
 	$chrsub = $chr;
 	$chrsub =~ s/_/-/g;
-	print $tempscript "/share/apps/samtools/samtools view -b -o $newfilepath/primary_$outfileid"."_$chrsub"."_visible_bam file.bam $chr &\n";
+	print $tempscript "/share/apps/samtools/samtools view -b -o primary_$outfileid"."_$chrsub"."_visible_bam file.bam $chr &\n";
 }
 
 print $tempscript "wait\n";
 close ($tempscript);
 
 system ("sh tempscript.sh");
-system ("rm -rf $tempfilepath");
